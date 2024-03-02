a6af548ae3baa35742fc973b741543a6e796dc76
Nbhujbal123 <157515364+Nbhujbal123@users.noreply.github.com>
2024-03-02 04:03:44 -0800

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function SecondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currfolder = folder;
-    let a = await fetch(`/${folder}/`);
+    let a = await fetch(`/songs/${folder}/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
