319abe56138ab08253cd610545f94105061be134
Nbhujbal123 <157515364+Nbhujbal123@users.noreply.github.com>
2024-03-03 23:16:46 -0800

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function SecondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currfolder = folder;
-    let a = await fetch(`/spotifycln/songs/${folder}/`);
+    let a = await fetch(`https://nbhujbal123.github.io/newproject/songs/${folder}/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
